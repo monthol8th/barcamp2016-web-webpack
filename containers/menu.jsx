d414f51f523720b7b37e64cfd9c53edca3492574
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-09-21 13:34:00 +0700

    Add menu in left menu

--- containers/menu.jsx
+++ containers/menu.jsx
@@ -3,7 +3,7 @@ var Slide = Menu.push;
 class Menus extends React.Component {
 	render() {
 		return (
-			<div >
+			<div>
 				<nav className="uk-navbar flex flex-center-x" data-uk-sticky>
 					<ul className="uk-navbar-nav uk-hidden-small">
 						<li>
@@ -35,9 +35,32 @@ class Menus extends React.Component {
 				</nav>
 				<div id="offcanvas-1" className="uk-offcanvas" aria-hidden="true">
 					<div className="uk-offcanvas-bar" mode="push">
-						<div className="uk-panel">Lorem ipsum dolor sit amet,
-							<a href="#">consetetur</a>
-							sadipscing elitr.</div>
+						<div className="uk-panel">
+							<li>
+								<a className="scroll" href="#index-section">bcbk</a>
+							</li>
+							<li>
+								<a className="scroll" href="#register-section">Register</a>
+							</li>
+							<li>
+								<a className="scroll" href="#schedule-section">Schedule</a>
+							</li>
+							<li>
+								<a className="scroll" href="#location-section">Location</a>
+							</li>
+							<li>
+								<a className="scroll" href="#sponsors-section">Sponsors</a>
+							</li>
+							<li>
+								<a href="">Resend Email</a>
+							</li>
+							<li>
+								<a href="">Who's Coming</a>
+							</li>
+							<li>
+								<a href="">Sessions</a>
+							</li>
+						</div>
 					</div>
 				</div>
 			</div>
